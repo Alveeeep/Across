874e0245241e7e78db2558a5d81ab00b05e6387a
Alveeeep <alvir.misalimov@gmail.com>
2021-09-28 18:15:36 +0300

    рабочая версия с sideNavigation

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("Sidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("Sidenav").style.width = "0";
+}
